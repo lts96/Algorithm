bd54bebdfdbc187275d98260bcc2cbc1a64f0685
lts96 <48347197+lts96@users.noreply.github.com>
2022-07-15 23:05:34 +0900

    Create 24479.kt

---
+++ BOJ/kotlin/24479.kt
@@ -0,0 +1,32 @@
+import java.io.*
+
+val max = 100001
+var visit = Array(max) {0}
+var arr : Array<ArrayList<Int>> = Array(max + 1) { ArrayList(0) }
+var seq = 0
+fun dfs(idx : Int){
+    if (visit[idx] != 0)
+        return
+    seq += 1
+    visit[idx] = seq
+    for (i in arr[idx]){
+        if (visit[i] != 0)
+            continue
+        dfs(i)
+    }
+}
+fun main(args: Array<String>) {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val (n, m, start) = br.readLine().split(' ').map { it.toInt() }
+    for (i in 1..m)
+    {
+        val (a, b) = br.readLine().split(' ').map { it.toInt() }
+        arr[a].add(b)
+        arr[b].add(a)
+    }
+    for (i in 1..n)
+        arr[i].sort()
+    dfs(start)
+    for (i in 1..n)
+        println(visit[i])
+}
